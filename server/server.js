24725add391cedd3f49fb37678f0ee4061d9cafc
Mohaimen Chowdhury Mahi <mmc.dp11@gmail.com>
2017-10-17 01:08:51 +0600

    Passing Room Data

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessege, generateLocationMessage} = require('./utils/messege');
+const {isRealString} = require('./utils/validation');
 const pathPublic = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -18,6 +19,14 @@ io.on('connection', (socket) => {
   socket.emit('newMessege', generateMessege('Admin','Welcome to the chat app'));
 
   socket.broadcast.emit('newMessege', generateMessege('Admin', 'New user connected'));
+
+  socket.on('join', (params, callback) => {
+    if(!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and Room is required');
+    }
+
+    callback();
+  });
 
   socket.on('createMessege', (messege, callback) => { // callback is for acknowledgement
     console.log('createMessege', messege);
